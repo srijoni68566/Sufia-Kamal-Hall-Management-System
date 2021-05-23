9bde3af6d276573e99db2cf04fd4ae09b532b924
Srijoni Saha <53531786+srijoni68566@users.noreply.github.com>
2021-05-23 12:52:27 -0700

    Add files via upload

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,36 @@
+<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+
+					<li><a href="#"><i class="fa fa-hotel"></i> Rooms</a>
+					<ul>
+						<li><a href="create-room.php">Add a Room</a></li>
+						<li><a href="manage-rooms.php">Manage Rooms</a></li>
+						<!-- <li><a href="manage-rooms.php">Manage Dining</a></li> -->
+					</ul>
+				</li>
+
+				<li><a href="#"><i class="fa fa-desktop"></i>Food </a>
+					<ul>
+						<li><a href="cfood.php">Add a Food</a></li>
+						<li><a href="manage-food.php">Manage Foods</a></li>
+						
+					</ul>
+				</li>
+
+
+				<li><a href="registration.php"><i class="fa fa-user"></i>User Registration</a></li>
+				<li><a href="manage-students.php"><i class="fa fa-users"></i>Manage Students</a></li>
+				<li><a href="Adining.php"><i class="glyphicon glyphicon-cutlery"></i> Dining</a></li>
+				<li><a href="#"><i class="fa fa-money"></i>Rent</a>
+					<ul>
+						<li><a href="rent_verify.php">Verify rent</a></li>
+						<li><a href="rent_status.php">Rent status</a></li>
+						<!-- <li><a href="manage-rooms.php">Manage Dining</a></li> -->
+					</ul>
+				</li>
+
+			
+		</nav>
